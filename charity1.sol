6ddf08cf24677318c7817263a70889847a9ba5c4
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-07-06 23:32:26 +0530

    Variables + Owner in Constructor

--- charity1.sol
+++ charity1.sol
@@ -2,3 +2,14 @@
 
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.18;
+
+contract Charity {
+    address public owner;
+    mapping(address => uint256) public balances;
+    event DonationReceived(address indexed donor, uint256 amount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+}
