9e60bad4f2eedf5325f23b45a3d4883921043d28
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-07-06 23:33:11 +0530

    modifier added

--- charity1.sol
+++ charity1.sol
@@ -11,5 +11,9 @@ contract Charity {
     constructor() {
         owner = msg.sender;
     }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
 
 }
